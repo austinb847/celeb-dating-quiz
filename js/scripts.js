89af761444f104f618f23c5e8704226c5c41e986
austinb847 <austinb847@icloud.com>
2020-03-26 13:26:38 -0700

    fix css style

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,25 @@ $(document).ready(function() {
       if (userGender === "male" && userZodiac === "1") {
         $("span.name").text(firstName);
         $("#match3").show();
+      
+      } else if (userGender === "male" && userZodiac === "2") {
+        $("span.name").text(firstName);
+        $("#match4").show();
+      
+      } else if (userGender === "female" && userZodiac === "3") {
+        $("span.name").text(firstName);
+        $("#match1").show();
+      
+      } else if (userGender === "female" && userZodiac === "4") {
+        $("span.name").text(firstName);
+        $("#match2").show();
+      } else {
+        alert("Sorry no match for you! :(");
       }
     
     } else {
       alert("Enter your first name please");
     }
-
-
     event.preventDefault();
   });
 
